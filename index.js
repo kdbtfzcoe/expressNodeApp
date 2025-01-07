0151b808e6dece1daebb75a9f7893e2ae1b84aef
Jeimarie Casey Pedraya <pedrayacasey08@gmail.com>
2025-01-07 23:11:33 +0800

    feat: :sparkles: trying another html

--- index.js
+++ index.js
@@ -232,6 +232,8 @@ const submitBookReqForm = require('./API/submit');
 
 app.use("/submit", submitBookReqForm);
 
+/*
+
 // start the server locally
 
 const PORT = 5000;
@@ -240,15 +242,15 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
+*/
 
 
 // start the server microsoft azure
 
-// const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-// server.listen(PORT, () => {
-  //  console.log(`Server running on port ${PORT}`);
-// })
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
 
 
